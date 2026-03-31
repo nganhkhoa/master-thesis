adf286c0d358d7ca571a17854c7060efe93b59cc
nganhkhoa <service.nganhkhoa@gmail.com>
2026-03-31 01:36:49 -0500

    ci: update

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 mkdir -p fonts
 pushd fonts
 
-wget https://github.com/alerque/libertinus/releases/download/v7.051/Libertinus-7.051.zip
-unzip Libertinus-7.051.zip
-mv Libertinus-7.051 libertinus
-
 wget https://font.download/dl/font/minion-pro.zip
 unzip -d minion-pro minion-pro.zip
 
 wget https://font.download/dl/font/latin-modern-mono.zip
 unzip -d lmmono latin-modern-mono.zip
+
+# not needed, but just download as well
+wget https://font.download/dl/font/stix-two-text.zip
+unzip -d stix-two stix-two-text.zip
+
+wget https://font.download/dl/font/stix-two-math.zip
+unzip -d stix-two stix-two-math.zip
 
 rm -rf *.zip
 
