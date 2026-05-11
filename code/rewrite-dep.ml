f54219c0b564db977822a89f67f224ac38ef6993
nganhkhoa <service.nganhkhoa@gmail.com>
2026-05-11 02:58:04 -0500

    ci: update

--- code/rewrite-dep.ml
+++ code/rewrite-dep.ml
@@ -1,15 +1,22 @@
 (* original program *)
-let [@contract : xpred -> outpred dep]
-  f (x : x_type) : f_type = body
+let [@contract :
+    (xpred * (gp1_p1_pred -> gp1_pred) -> gpred as 'dep) -> outpred as 'dep]
+  f (x : x_type) (g : g_type) : f_type = body
 
 
 (* rewritten program *)
-let f pos neg cloc (x : x_type) : f_type =
+let f pos neg cloc (x : x_type) (g : g_type) : f_type =
   let [@contract : xpred] x : x_type = x in
   let x_dep = x neg cloc cloc in
   let x = x neg pos cloc in
 
-  let outpred_dep = outpred x_dep in
+  let [@contract : (gp1_p1_pred -> gp1_pred) -> gpred as 'dep]
+    g (g_p1 : gp1_intype) : g_outtype = g g_p1 in
+  let g_dep =
+    fun p1 -> Contract.run_with_effects (fun () -> g neg cloc cloc p1) in
+  let g = g neg pos cloc in
+
+  let outpred_dep = outpred x_dep g_dep in
   let [@contract : outpred_dep] ret : f_type = body in
   ret pos neg cloc
 
--- code/rewrite-flat.ml
+++ code/rewrite-flat.ml
@@ -3,24 +3,4 @@ let [@contract : predicate] var : v_type = value
 
 (* rewritten program *)
 let var pos neg cloc =
-  let module Contract = struct
-    type _ Effect.t += V : v_type -> v_type Effect.t
-  end in
-
-  let handler =
-    {
-      effc = fun (type e) (eff : e Effect.t) ->
-        match eff with
-        | Contract.V x -> Some (fun k ->
-            if predicate x
-            then continue k x
-            else discontinue k (Blame pos)
-          )
-        | _ -> None
-    }
-  in
-
-  try_with (fun () ->
-    Effect.perform (Contract.V value);
-  ) ()
-  handler
+  Effect.perform (Contract.Flat (pos, predicate, value));
